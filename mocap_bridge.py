3d4b0765e860e5df700b53a94df6be2ac3c7ea11
Andreas Larsen <ankula@mp.aau.dk>
2025-07-27 18:42:20 +0200

    Bugfix: cm to m

--- mocap_bridge.py
+++ mocap_bridge.py
@@ -1,11 +1,14 @@
-import asyncio
-import qtm_rt
-from time import sleep, time
-from math import copysign
-from pymavlink import mavutil
-import xml.etree.ElementTree as ET
-from pprint import pformat
 import argparse
+import asyncio
+import xml.etree.ElementTree as ET
+from math import isnan
+from pprint import pformat
+from time import time
+from functools import partial
+
+import qtm_rt
+from pymavlink import mavutil
+from scipy.spatial.transform import Rotation as R
 
 
 def bodyToIndex(params: str) -> dict:
@@ -18,25 +21,9 @@ def bodyToIndex(params: str) -> dict:
     return body_to_index
 
 
-def rotToQuat(mat: tuple) -> tuple[float, ...]:
-    """Converts rotation matrix to quaternions.
-
-    Args:
-        mat (tuple): Rotation matrix as tuple.
-
-    Returns:
-        tuple: Normalized quaternion (r, i, j, k).
-    """
-    r11, r12, r13 = mat[0], mat[1], mat[2]
-    r21, r22, r23 = mat[3], mat[4], mat[5]
-    r31, r32, r33 = mat[6], mat[7], mat[8]
-
-    # Numerical errors leads to domain errors, thus, we max to avoid negatives
-    r = 0.5 * max(0, 1 + r11 + r22 + r33) ** 0.5
-    i = 0.5 * max(0, 1 + r11 - r22 - r33) ** 0.5 * copysign(1.0, r32 - r23)
-    j = 0.5 * max(0, 1 - r11 + r22 - r33) ** 0.5 * copysign(1.0, r13 - r31)
-    k = 0.5 * max(0, 1 - r11 - r22 + r33) ** 0.5 * copysign(1.0, r21 - r12)
-    return (r, i, j, k)
+def eulerToQuat(roll: float, pitch: float, yaw: float) -> list[float,]:
+    r = R.from_euler("xyz", [roll, pitch, yaw], degrees=True)
+    return r.as_quat(scalar_first=True).tolist()
 
 
 def initMav(mavlink_address: str, lat: int = 0, lon: int = 0, alt: int = 0) -> mavutil.mavtcp | mavutil.mavtcpin | mavutil.mavudp | mavutil.mavwebsocket | mavutil.mavmcast:
@@ -51,81 +38,95 @@ def initMav(mavlink_address: str, lat: int = 0, lon: int = 0, alt: int = 0) -> m
     Returns:
         mavutil.mavXXX: Serial, UDP, TCP, etc. mavlink connection
     """
+
     print("Connecting to MAV...")
     conn = mavutil.mavlink_connection(mavlink_address)
+
     print("Waiting for Heartbeat...")
     conn.wait_heartbeat()
-    print("Relaying system time...")
-    msg = mavutil.mavlink.MAVLink_system_time_message(time_unix_usec=int(time()), time_boot_ms=0)
-    conn.mav.send(msg)
-    lat, lon, alt = 0, 0, 0
+
+    # print("Relaying system time...")
+    # msg = mavutil.mavlink.MAVLink_system_time_message(time_unix_usec=int(time()), time_boot_ms=0)
+    # conn.mav.send(msg)
+
+    lat, lon, alt = 55.270884, 14.755208, 0
     print(f"Setting GPS Origin to {lat=}, {lon=}, {alt=}")
-    msg = mavutil.mavlink.MAVLink_gps_global_origin_message(
-        latitude=lat,
-        longitude=lon,
-        altitude=alt,
-        time_usec=int(time()),
-    )
+    msg = conn.mav.command_long_encode(
+            conn.target_system,
+            conn.target_component,
+            48,#mavutil.mavlink.MAV_CMD_SET_GLOBAL_ORIGIN,  # ID of command to send
+            0,              # Confirmation
+            0,              # (Use Current)
+            int(lat*1e7),   # (Latitude)
+            int(lon*1e7),   # (Longitude)
+            alt,            # (Altitude)
+            0,              # (Unused)
+            0,              # (Unused)
+            0,              # (Unused)
+        )
     conn.mav.send(msg)
 
     print("MavLink Initilized!")
     return conn
 
 
-async def initQTM(qtm_address):
-    conn = await qtm_rt.connect(qtm_address)
+def initQTM(qtm_address):
+    conn = qtm_rt.connect(qtm_address)
+
     if conn is None:
         raise Exception("Failed to connect to QTM!")
-
+    
     # Get 6dof settings from qtm
-    params = await conn.get_parameters(parameters=["6d"])
+    params = conn.get_parameters(parameters=["6d"])
     body_index = bodyToIndex(params)
-
     print("Found rigid bodies:", pformat(body_index))
 
     print("QTM Initilized!")
     return conn, body_index
 
 
+def sendPosAtt(packet, body_index, conn_mav):
+        _, bodies = packet.get_6d_euler()
+        print(f"Errors:")
+        print(f"Framenumber: {packet.framenumber}")
+
+        wanted_index = body_index[RIGID_BODY]
+        position, rotation = bodies[wanted_index]
+        x, y, z = position.x, position.y, position.z
+        roll, pitch, yaw = rotation.a1, rotation.a2, rotation.a3
+        print(f"x={x:>11.5f}, y={y:>11.5f}, z={z:>11.5f}")
+        print(f"roll={roll:>11.5f}, pitch={pitch:>11.5f}, yaw={yaw:>11.5f}")
+        print(f"qw={qw:>11.5f}, qx={qx:>11.5f}, qy={qy:>11.5f}, qz={qz:>11.5f}")
+
+        if isnan(x) or isnan(y) or isnan(z) or isnan(roll) or isnan(pitch) or isnan(yaw):
+            print("\33[5A", end="")
+            print(f'Errors: QTM LOST TRACK OF BODY "{RIGID_BODY}" AT FRAME {packet.framenumber}!', end="")
+            print("\33[5B", end="")
+            return
+
+        qw, qx, qy, qz = eulerToQuat(roll, pitch, yaw)
+        msg = mavutil.mavlink.MAVLink_att_pos_mocap_message(
+            time_usec=int(time()),
+            q=[qw, qy, qx, qz],
+            x=x / 1000, # Qualisys reports in cm
+            y=y / 1000,
+            z=z / 1000,
+        )
+        conn_mav.mav.send(msg)
+        if not VERBOSE:
+            # Move cursor n (\33[nA) lines up => Overwrites output and does not spam console
+            print("\33[5A", end="")
+
 async def main():
 
     conn_mav = initMav(MAVLINK_ADDRESS)
-    conn_qtm, body_index = initQTM(QTM_ADDRESS)
+    conn_qtm, body_index = await initQTM(QTM_ADDRESS)
 
-    def on_packet(packet):
-        _, bodies = packet.get_6d()
-        print(f"Framenumber: {packet.framenumber}")
-
-        if RIGID_BODY is not None and RIGID_BODY in body_index:
-
-            wanted_index = body_index[RIGID_BODY]
-            position, rotation = bodies[wanted_index]
-            x, y, z = position.x, position.y, position.z
-            mat = rotation.matrix
-            r, i, j, k = rotToQuat(mat)
-            print(f"x={x:>10.5f}, y={y:>10.5f}, z={z:>10.5f}")
-            print(f"q={r:>10.5f}, i={i:>10.5f}, j={j:>10.5f}, k={k:>10.5f}")
-            if VERBOSE:
-                print(f"Rot: {mat[0]:>10.5f}, {mat[1]:>10.5f}, {mat[2]:>10.5f}")
-                print(f"     {mat[3]:>10.5f}, {mat[4]:>10.5f}, {mat[5]:>10.5f}")
-                print(f"     {mat[6]:>10.5f}, {mat[7]:>10.5f}, {mat[8]:>10.5f}")
-            else:
-                # Move cursor n (\33[nA) lines up => Overwrites output and does not spam console
-                print("\33[3A", end="")
-
-            msg = mavutil.mavlink.MAVLink_att_pos_mocap_message(
-                time_usec=int(time()),
-                q=[r, i, j, k],
-                x=x,
-                y=y,
-                z=z,  # TODO: Check if z should be inverted
-            )
-            conn_mav.mav.send(msg)
-
-        else:
-            raise Exception(f"Rigid Body {RIGID_BODY} was not found!")
-
-    await conn_qtm.stream_frames(frames=f"frequency:{MAVLINK_FREQUENCY}", components=["6d"], on_packet=on_packet)
+    if RIGID_BODY is None and RIGID_BODY not in body_index:
+        raise Exception(f"Rigid Body {RIGID_BODY} was not found!")
+    
+    
+    await conn_qtm.stream_frames(frames=f"frequency:{MAVLINK_FREQUENCY}", components=["6d"], on_packet=partial(sendPosAtt, body_index, conn_mav))
 
 
 if __name__ == "__main__":
@@ -139,15 +140,15 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--body", default="mav")
     parser.add_argument("-qtm", "--qtm_address", default="127.0.0.1")
     parser.add_argument("-mav", "--mavlink_address", default="tcp:192.168.2.1:5760")
-    parser.add_argument("-f", "--frequency", default=5)
-    parser.add_argument("-v", "--verbose", default=True)
+    parser.add_argument("-f", "--frequency", default=10)
+    parser.add_argument("-v", "--verbose", default=False)
 
     args = parser.parse_args()
 
-    RIGID_BODY = args.body  # "mav"  As named in QTM
-    QTM_ADDRESS = args.qtm_address  # "127.0.0.1"
-    MAVLINK_ADDRESS = args.mavlink_address  # "tcp:192.168.2.1:5760"
-    MAVLINK_FREQUENCY = args.frequency  # 5 Hz
+    RIGID_BODY = args.body
+    QTM_ADDRESS = args.qtm_address
+    MAVLINK_ADDRESS = args.mavlink_address
+    MAVLINK_FREQUENCY = args.frequency
     VERBOSE = args.verbose
 
     asyncio.ensure_future(main())
